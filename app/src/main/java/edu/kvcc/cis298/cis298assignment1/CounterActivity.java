bde9283d6ba7ba9798d6ddf6880850b95fe4b44a
Chad Cunningham <Chad Cunningham>
2016-09-20 11:10:18 -0400

    Add plus/minus buttons, text view, and logic to make them work.

--- app/src/main/java/edu/kvcc/cis298/cis298assignment1/CounterActivity.java
+++ app/src/main/java/edu/kvcc/cis298/cis298assignment1/CounterActivity.java
@@ -4,14 +4,72 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CounterActivity extends AppCompatActivity {
+
+    private TextView mCounterTextView;
+    private Button mIncrementButton;
+    private Button mDecrementButton;
+
+    private int mCounterValue = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_counter);
+
+        // >Use findViewById to get a reference to the textview in the layout.
+        mCounterTextView = (TextView) findViewById(R.id.counter_text_view);
+       // >Change the counter view.
+        updateCounterView();
+
+        // >Get a reference to the increment button and set it's onclicklistener.
+        mIncrementButton = (Button) findViewById(R.id.increment_button);
+        mIncrementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                incrementCounter();
+            }
+        });
+        // >Get a reference to the decrement button and set it's onclicklistener.
+        mDecrementButton = (Button) findViewById(R.id.decrement_button);
+        mDecrementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                decrementCounter();
+            }
+        });
+
     }
+
+    // >Change the text view to the current counter value.
+    private void updateCounterView() {
+        mCounterTextView.setText(Integer.toString(mCounterValue));
+    }
+
+    // >Increment the counter.
+    private void incrementCounter() {
+        mCounterValue++;
+        updateCounterView();
+        if (mCounterValue == 21) {
+            Toast.makeText(this, "The counter is over 20!", Toast.LENGTH_SHORT).show();
+        }
+    }
+    // >Decrement the counter.
+    private void decrementCounter() {
+        mCounterValue--;
+        updateCounterView();
+    }
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
